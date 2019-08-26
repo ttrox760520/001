718c701943125a274c463b48e2ce80df3db2cf41
ttrox760520 <ttrox760520@gmail.com>
2019-08-26 13:41:11 +0800

    2nd version

---
+++ WebApplication1/WebApplication1/Models/UserInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UserInfoViewModel
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
